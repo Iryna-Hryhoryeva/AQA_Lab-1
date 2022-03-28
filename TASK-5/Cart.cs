ac18707042c97cf6569069d07b5b977616397c76
Iryna Hryhoryeva <irynahryhoryeva@yandex.by>
2022-03-28 06:29:59 +0300

    TASK-5: Put constructor b/w fields and getters&setters in class Cart

--- TASK-5/Cart.cs
+++ TASK-5/Cart.cs
@@ -4,10 +4,14 @@ public class Cart
 {
     private List<Ware> _wares;
 
-    public List<Ware> Wares { get => _wares; set => _wares = value; }
-
     public Cart()
     {
         Wares = new List<Ware>();
+    }
+
+    public List<Ware> Wares
+    {
+        get => _wares;
+        set => _wares = value;
     }
 }
